d97bc10ea63ac20da614ad5611c2dc2d62e92850
samofoke <samofoke@student.wethinkcode.co.za>
2020-04-25 23:59:24 +0200

    adding the functionality to add numbers

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +26,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                TextView textValue = findViewById(R.id.text_value);
+                String stringValue = textValue.getText().toString();
+                int originalValue = Integer.parseInt(stringValue);
+                int newValue = MyWorker.doubleTheValue(originalValue);
+                textValue.setText(Integer.toString(newValue));
+
+                Snackbar.make(view, "Change Value " + originalValue + " to " + newValue, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
---
+++ app/src/main/java/com/example/myapp/MyWorker.java
@@ -0,0 +1,7 @@
+package com.example.myapp;
+
+public class MyWorker {
+    public static int doubleTheValue(int value) {
+        return value * 2;
+    }
+}
